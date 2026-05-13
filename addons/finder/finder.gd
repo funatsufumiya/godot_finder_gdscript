c87f6640d63d29c943d8b22a2f486d8947604025
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-05-13 18:35:26 +0900

    wip

--- addons/finder/finder.gd
+++ addons/finder/finder.gd
@@ -16,15 +16,40 @@ func find_child_by_name(root: Node, name: String):
 			return gp
 	return null
 	
+func is_node_of_class(node: Node, class_string: String) -> bool:
+	var is_instance := false
+	if node.get_script():
+		if node.get_script().get_global_name() == class_string:
+			is_instance = true
+	if node.is_class(class_string): # Keep this check in for built-in classes
+		is_instance = true
+	return is_instance
+	
 func find_child_by_type(root: Node, typename: String):
 	if root == null:
-		push_warning("find_child_by_name: parent is null")
+		push_warning("find_child_by_type: parent is null")
 		return null
 	
 	for p in root.get_children():
-		if p.is_class(typename):
+		if is_node_of_class(p, typename):
 			return p
 		var gp = find_child_by_type(p, typename)
 		if gp != null:
 			return gp
 	return null
+	
+func find_children_by_type(root: Node, typename: String):
+	if root == null:
+		push_warning("find_children_by_type: parent is null")
+		return null
+		
+	var res = []
+	
+	for p in root.get_children():
+		if is_node_of_class(p, typename):
+			res.append(p)
+		if p.get_child_count() > 0:
+			var gps = find_children_by_type(p, typename)
+			for gp in gps:
+				res.append(gp)
+	return res
